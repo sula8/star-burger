b826e27bef36faa3a6860ff0744c51fbbec04272
Sultanbek Altayev <sultanbekaltayev8@gmail.com>
2020-07-26 20:51:56 +0100

    add fetch_coordinates tool

---
+++ foodcartapp/utils.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+
+def fetch_coordinates(place):
+    apikey = os.getenv("YA_APIKEY")
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    params = {"geocode": place, "apikey": apikey, "format": "json"}
+    response = requests.get(base_url, params=params)
+    response.raise_for_status()
+    places_found = response.json()['response']['GeoObjectCollection']['featureMember']
+    most_relevant = places_found[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return float(lat), float(lon)
