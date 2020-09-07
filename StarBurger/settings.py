4fdf83b3525cd34b40d2dd2108e5c87c05db01a0
Sultanbek Altayev <sultanbekaltayev8@gmail.com>
2020-09-07 16:48:56 +0100

    changed cache settings

--- StarBurger/settings.py
+++ StarBurger/settings.py
@@ -87,9 +87,12 @@ DATABASES = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
-        "LOCATION": "cache_table",
-        "TIMEOUT": None,
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "star"
     }
 }
 
